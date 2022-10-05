e2ebae6c17ee7cb0ba7e7207982a6d38fcfff248
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-05 11:11:06 +0300

    add the navlinks to the navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,26 @@
  import React from 'react'
- 
+ import {Container, Button, Nav, Navbar as BootsrapNav} from "react-bootstrap"
+ import {NavLink} from "react-router-dom"
  export default function Navbar() {
    return (
-     <div>Navbar</div>
+     <div>
+      <BootsrapNav className='bg-white shadow-sm mb-3'>
+        <Container>
+          <Nav className='ne-auto'>
+            <Nav.Link to="/" as={NavLink}>
+              Home
+            </Nav.Link>
+            <Nav.Link to="/about" as={NavLink}>
+              About
+            </Nav.Link>
+            <Nav.Link to="/products" as={NavLink}>
+              Products
+            </Nav.Link>
+          </Nav>
+          <Button></Button>
+        </Container>
+      </BootsrapNav>
+      </div>
    )
  }
  
