f7417d80bf3889125719902b80197ca8f8cde4ae
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-06 11:31:19 +0300

    modify products component

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Col, Row } from "react-bootstrap"
+import productItems from "../data/db.json"
+import ProductList from '../components/ProductList'
+
 
 export default function Products() {
+
   return (
-    <div>Products</div>
+    <>
+      <h1>Products</h1>
+      
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {productItems.map( prod => {
+          return(
+            <Col key={prod.id}>
+              <ProductList
+              {...prod}
+              />
+
+            </Col>
+          )
+        }
+          
+        )}
+      </Row>
+    </>
   )
 }
